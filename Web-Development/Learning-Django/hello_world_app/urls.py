5b47055061e06e33e085cbfcfc475b899005c92a
Oliver Keen <72150727+oliverkeen@users.noreply.github.com>
2021-03-24 21:57:43 -0400

    Fixed a bug in urls.py, fixed a typo, and added photo of site in action

--- Web-Development/Learning-Django/hello_world_app/urls.py
+++ Web-Development/Learning-Django/hello_world_app/urls.py
@@ -3,5 +3,5 @@ from hello_world_app import views
 
 # Creates list of URL patterns corresponding to various view functions
 urlpatterns = [
-    path("", views.hello_world_app, name="hello_world_app"),
+    path("", views.hello_world, name="hello_world"),
 ]
